9e317419352342d635244d83defb777f4f288d80
xayal12123 <xayalamanov121@gmail.com>
2026-05-12 00:38:03 +0400

    Refactor MASTER_CATALOG structure and categories

--- katalok.js
+++ katalok.js
@@ -1,129 +1,59 @@
+// MASTER KATALOG - TAM GENİŞ VERSİYA (Bütün skrinşotlar və çatışmayan bölmələr daxil)
 window.MASTER_CATALOG = {
-    "Elektronika": {
-        img: "https://yazarlar.az/assets/img/cat-elek.png",
-        list: {
-            "Bütün elanlar": [],
-            "Telefonlar": ["Apple iPhone", "Samsung", "Xiaomi", "Nokia", "Honor", "Huawei"],
-            "Audio və video": [],
-            "Kompüter aksesuarları": [],
-            "Oyunlar, pultlar və proqramlar": [],
-            "Masaüstü kompüterlər": [],
-            "Noutbuklar və netbuklar": [],
-            "Planşet və elektron kitablar": [],
-            "Fototexnika": []
-        }
-    },
-    "Nəqliyyat": {
-        img: "https://yazarlar.az/assets/img/cat-car.png",
-        list: {
-            "Bütün elanlar": [],
-            "Avtomobillər": [],
-            "Ehtiyat hissələri və aksesuarlar": [],
-            "Motosikletlər və mopedlər": [],
-            "Tikinti texnikası": [],
-            "Aqrotexnika": [],
-            "Avtobuslar": [],
-            "Yük maşınları və qoşqular": [],
-            "Qeydiyyat nişanları": []
-        }
-    },
-    "Daşınmaz əmlak": {
-        img: "https://yazarlar.az/assets/img/cat-home.png",
-        list: {
-            "Bütün elanlar": [],
-            "Mənzillər": ["Yeni tikili", "Köhnə tikili", "Kirayə"],
-            "Həyət evləri, bağ evləri": [],
-            "Torpaq": [],
-            "Obyektlər və ofislər": [],
-            "Qarajlar": [],
-            "Xaricdə əmlak": []
-        }
-    },
-    "Ev və bağ üçün": {
-        img: "https://yazarlar.az/assets/img/cat-mebel.png",
-        list: {
-            "Təmir və tikinti": [],
-            "Mebellər": [],
-            "Məişət texnikası": [],
-            "Qab-qacaq və mətbəx ləvazimatları": [],
-            "Bitkilər": [],
-            "Xalçalar və aksesuarlar": [],
-            "Ev tekstili": [],
-            "Dekor və interyer": [],
-            "Bağ və bostan": []
-        }
-    },
-    "Məişət texnikası": {
-        img: "https://img.tap.az/t/p/w200/400977287.jpg",
-        list: {
-            "Paltaryuyanlar": [],
-            "Soyuducular": [],
-            "Kondisionerlər": [],
-            "Mikrodalğalı sobalar": [],
-            "Tozsoranlar": [],
-            "Ütülər": [],
-            "Qabyuyanlar": [],
-            "Mətbəx kombaynları": [],
-            "Elektrik çaydanlar": []
-        }
-    },
-    "Şəxsi əşyalar": {
-        img: "https://yazarlar.az/assets/img/cat-wear.png",
-        list: {
-            "Geyim": ["Kişi geyimi", "Qadın geyimi"],
-            "Ayaqqabılar": ["Kişi ayaqqabıları", "Qadın ayaqqabıları"],
-            "Aksesuarlar": ["Saatlar", "Zinət əşyaları", "Çantalar"],
-            "Sağlamlıq və gözəllik": ["Ətirlər", "Kosmetika"]
-        }
-    },
-    "Uşaq aləmi": {
-        img: "https://yazarlar.az/assets/img/cat-child.png",
-        list: {
-            "Oyuncaqlar": [],
-            "Uşaq arabaları": [],
-            "Uşaq geyimi": [],
-            "Uşaq mebeli": [],
-            "Məktəblilər üçün": [],
-            "Çarpayılar və beşiklər": []
-        }
-    },
-    "Heyvanlar": {
-        img: "https://img.tap.az/t/p/w200/397368291.jpg",
-        list: {
-            "İtlər": [],
-            "Pişiklər": [],
-            "Quşlar": [],
-            "Akvariumlar və balıqlar": [],
-            "Atlar": [],
-            "K/t heyvanları": []
-        }
-    },
-    "İş elanları": {
-        img: "https://yazarlar.az/assets/img/cat-work.png",
-        list: {
-            "Vakansiyalar": [],
-            "İş axtarıram": []
-        }
-    },
-    "Hobbi və asudə": {
-        img: "https://yazarlar.az/assets/img/cat-sport.png",
-        list: {
-            "Biletlər və səyahət": [],
-            "Velosipedlər": [],
-            "İdman və asudə": [],
-            "Musiqi alətləri": [],
-            "Kitab və jurnallar": [],
-            "Kolleksiyalar": []
-        }
-    },
-    "Xidmətlər": {
-        img: "https://yazarlar.az/assets/img/cat-biz.png",
-        list: {
-            "Təmir və tikinti xidmətləri": [],
-            "Nəqliyyat xidmətləri": [],
-            "Təhsil və kurslar": [],
-            "Gözəllik və sağlamlıq": [],
-            "Biznes xidmətləri": []
-        }
-    }
+  "Elektronika": { 
+    img: "https://images.unsplash.com/photo-1550009158-9ebf69173e03?w=400", 
+    subs: ["Bütün elanlar", "Telefonlar", "Apple iPhone", "Samsung", "Xiaomi", "Noutbuklar", "Planşetlər", "Audio və video", "Televizorlar", "Oyun konsolları", "Foto-aparatlar", "Smart saatlar", "Nömrələr və SIM-kartlar", "Masaüstü kompüterlər", "Komponentlər və monitorlar", "Şəbəkə avadanlığı"] 
+  },
+  "Nəqliyyat": { 
+    img: "https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?w=400", 
+    subs: ["Bütün elanlar", "Avtomobillər", "Ehtiyat hissələri və aksesuarlar", "Şinlər və disklər", "Motosikletlər və mopedlər", "Yük maşınları", "Tikinti texnikası", "Aqrotexnika", "Qeydiyyat nişanları", "Su nəqliyyatı"] 
+  },
+  "Daşınmaz əmlak": { 
+    img: "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?w=400", 
+    subs: ["Bütün elanlar", "Mənzillər", "Yeni tikililər", "Köhnə tikililər", "Həyət evləri, bağ evləri", "Torpaq sahələri", "Obyektlər", "Ofislər", "Qarajlar", "Xaricdə əmlak"] 
+  },
+  "Məişət texnikası": { 
+    img: "https://images.unsplash.com/photo-1556911220-e15b29be8c8f?w=400", 
+    subs: ["Bütün elanlar", "Soyuducular", "Paltaryuyanlar", "Tozsoranlar", "Ütülər", "Süd separatorları", "Vakuum paketləyicilər", "Tərəzilər", "Termometrlər", "Su dispenserləri", "Kondisionerlər", "Tikiş maşınları", "Aspiratorlar", "Mikrodalğalı sobalar", "Blenderlər və mikserlər"] 
+  },
+  "Ev və bağ üçün": { 
+    img: "https://images.unsplash.com/photo-1513584684374-8bdb74838a0f?w=400", 
+    subs: ["Bütün elanlar", "Təmir və tikinti", "Mebellər", "Qab-qacaq", "Bitkilər", "Ərzaq", "Xalçalar", "Ev tekstili", "İşıqlandırma", "Dekor və interyer", "Bağ və bostan"] 
+  },
+  "Ehtiyat hissələri və aksesuarlar": { 
+    img: "https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?w=400", 
+    subs: ["Bütün elanlar", "Aksesuarlar", "Audio və video texnika", "Avto kosmetika və kimya", "Avtomobil üçün alətlər", "Diaqnostika cihazları", "Ehtiyat hissələri", "GPS-naviqatorlar", "Şinlər, disklər və təkərlər", "Siqnalizasiyalar", "Videoqeydiyyatçılar"] 
+  },
+  "Uşaq aləmi": { 
+    img: "https://images.unsplash.com/photo-1515488764276-38520b212896?w=400", 
+    subs: ["Bütün elanlar", "Oyuncaqlar", "Uşaq arabaları", "Uşaq geyimi", "Uşaq mebeli", "Manejlər", "Beşiklər", "Sürüşkənlər", "Hammam və gigiyena", "Uşaq avtomobilləri", "Yürütəclər"] 
+  },
+  "Şəxsi əşyalar": { 
+    img: "https://images.unsplash.com/photo-1445205170230-053b830c6050?w=400", 
+    subs: ["Bütün elanlar", "Geyim və ayaqqabılar", "Saatlar və zinət əşyaları", "Aksesuarlar", "Sağlamlıq və gözəllik", "Ətirlər"] 
+  },
+  "Heyvanlar": { 
+    img: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?w=400", 
+    subs: ["Bütün elanlar", "İtlər", "Pişiklər", "Quşlar", "Arıçılıq", "K/t heyvanları", "Akvariumlar", "Atlar", "Dovşanlar", "Heyvanlar üçün məhsullar"] 
+  },
+  "Hobbi və asudə": { 
+    img: "https://images.unsplash.com/photo-1500485035595-cbe6f645feb1?w=400", 
+    subs: ["Bütün elanlar", "Velosipedlər", "Musiqi alətləri", "İdman və asudə", "Kitab və jurnallar", "Kempinq və balıqçılıq", "Kolleksiyalar", "Biletlər və səyahət", "Tanışlıq"] 
+  },
+  "Xidmətlər və biznes": { 
+    img: "https://images.unsplash.com/photo-1454165833767-1390e44a17d5?w=400", 
+    subs: ["Bütün elanlar", "Təmir və tikinti", "Nəqliyyat xidmətləri", "Tədris və kurslar", "İT və dizayn", "Tibbi xidmətlər", "Avadanlıqların icarəsi", "Maliyyə və hüquq"] 
+  },
+  "İş elanları": { 
+    img: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?w=400", 
+    subs: ["Vakansiyalar", "İş axtarıram"] 
+  },
+  "Məktəblilər üçün": { 
+    img: "https://images.unsplash.com/photo-1503676260728-1c00da094a0b?w=400", 
+    subs: ["Bütün elanlar", "Dəftərxana ləvazimatları", "Dərsliklər", "Məktəbli çantaları", "Məktəbli forması", "Yaradıcılıq"] 
+  },
+  "Mağazalar": { 
+    img: "https://images.unsplash.com/photo-1441986300917-64674bd600d8?w=400", 
+    subs: ["Bütün mağazalar"] 
+  }
 };
