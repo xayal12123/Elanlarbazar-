1526eee0ce8c90b6f4e74bc0d37c2817c0dee280
xayal12123 <xayalamanov121@gmail.com>
2026-05-12 00:24:08 +0400

    Add MASTER_CATALOG with categories and items

---
+++ katalok.js
@@ -0,0 +1,129 @@
+window.MASTER_CATALOG = {
+    "Elektronika": {
+        img: "https://yazarlar.az/assets/img/cat-elek.png",
+        list: {
+            "Bütün elanlar": [],
+            "Telefonlar": ["Apple iPhone", "Samsung", "Xiaomi", "Nokia", "Honor", "Huawei"],
+            "Audio və video": [],
+            "Kompüter aksesuarları": [],
+            "Oyunlar, pultlar və proqramlar": [],
+            "Masaüstü kompüterlər": [],
+            "Noutbuklar və netbuklar": [],
+            "Planşet və elektron kitablar": [],
+            "Fototexnika": []
+        }
+    },
+    "Nəqliyyat": {
+        img: "https://yazarlar.az/assets/img/cat-car.png",
+        list: {
+            "Bütün elanlar": [],
+            "Avtomobillər": [],
+            "Ehtiyat hissələri və aksesuarlar": [],
+            "Motosikletlər və mopedlər": [],
+            "Tikinti texnikası": [],
+            "Aqrotexnika": [],
+            "Avtobuslar": [],
+            "Yük maşınları və qoşqular": [],
+            "Qeydiyyat nişanları": []
+        }
+    },
+    "Daşınmaz əmlak": {
+        img: "https://yazarlar.az/assets/img/cat-home.png",
+        list: {
+            "Bütün elanlar": [],
+            "Mənzillər": ["Yeni tikili", "Köhnə tikili", "Kirayə"],
+            "Həyət evləri, bağ evləri": [],
+            "Torpaq": [],
+            "Obyektlər və ofislər": [],
+            "Qarajlar": [],
+            "Xaricdə əmlak": []
+        }
+    },
+    "Ev və bağ üçün": {
+        img: "https://yazarlar.az/assets/img/cat-mebel.png",
+        list: {
+            "Təmir və tikinti": [],
+            "Mebellər": [],
+            "Məişət texnikası": [],
+            "Qab-qacaq və mətbəx ləvazimatları": [],
+            "Bitkilər": [],
+            "Xalçalar və aksesuarlar": [],
+            "Ev tekstili": [],
+            "Dekor və interyer": [],
+            "Bağ və bostan": []
+        }
+    },
+    "Məişət texnikası": {
+        img: "https://img.tap.az/t/p/w200/400977287.jpg",
+        list: {
+            "Paltaryuyanlar": [],
+            "Soyuducular": [],
+            "Kondisionerlər": [],
+            "Mikrodalğalı sobalar": [],
+            "Tozsoranlar": [],
+            "Ütülər": [],
+            "Qabyuyanlar": [],
+            "Mətbəx kombaynları": [],
+            "Elektrik çaydanlar": []
+        }
+    },
+    "Şəxsi əşyalar": {
+        img: "https://yazarlar.az/assets/img/cat-wear.png",
+        list: {
+            "Geyim": ["Kişi geyimi", "Qadın geyimi"],
+            "Ayaqqabılar": ["Kişi ayaqqabıları", "Qadın ayaqqabıları"],
+            "Aksesuarlar": ["Saatlar", "Zinət əşyaları", "Çantalar"],
+            "Sağlamlıq və gözəllik": ["Ətirlər", "Kosmetika"]
+        }
+    },
+    "Uşaq aləmi": {
+        img: "https://yazarlar.az/assets/img/cat-child.png",
+        list: {
+            "Oyuncaqlar": [],
+            "Uşaq arabaları": [],
+            "Uşaq geyimi": [],
+            "Uşaq mebeli": [],
+            "Məktəblilər üçün": [],
+            "Çarpayılar və beşiklər": []
+        }
+    },
+    "Heyvanlar": {
+        img: "https://img.tap.az/t/p/w200/397368291.jpg",
+        list: {
+            "İtlər": [],
+            "Pişiklər": [],
+            "Quşlar": [],
+            "Akvariumlar və balıqlar": [],
+            "Atlar": [],
+            "K/t heyvanları": []
+        }
+    },
+    "İş elanları": {
+        img: "https://yazarlar.az/assets/img/cat-work.png",
+        list: {
+            "Vakansiyalar": [],
+            "İş axtarıram": []
+        }
+    },
+    "Hobbi və asudə": {
+        img: "https://yazarlar.az/assets/img/cat-sport.png",
+        list: {
+            "Biletlər və səyahət": [],
+            "Velosipedlər": [],
+            "İdman və asudə": [],
+            "Musiqi alətləri": [],
+            "Kitab və jurnallar": [],
+            "Kolleksiyalar": []
+        }
+    },
+    "Xidmətlər": {
+        img: "https://yazarlar.az/assets/img/cat-biz.png",
+        list: {
+            "Təmir və tikinti xidmətləri": [],
+            "Nəqliyyat xidmətləri": [],
+            "Təhsil və kurslar": [],
+            "Gözəllik və sağlamlıq": [],
+            "Biznes xidmətləri": []
+        }
+    }
+};
